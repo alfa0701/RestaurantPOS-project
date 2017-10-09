f419007cbfb237d718ea09329b650d044955a731
MihoLaptop <@home>
2017-10-08 23:17:12 -0400

    no message

--- OrderView.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE VIEW [dbo].[OrderByTableView]
-	AS SELECT m.MenueName, Sum(od.Qty)
-FROM dbo.[Order] as o
-INNER JOIN dbo.[OrderDetail] as od
-ON o.OrderId = od.OrderId
-INNER JOIN dbo.[Menue] as m
-ON m.MenuId = od.MenuId
-Group by od.MenuId ;
-
